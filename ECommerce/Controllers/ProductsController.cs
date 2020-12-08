13c2f2f1cf30ae417a48f5270e4a8d3094cf4d22
ekaterinatsmyg <ekaterina.tsmyg@gmail.com>
2020-12-08 10:11:33 +0300

    Initial commit

---
+++ ECommerce/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ECommerce.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductsController : ControllerBase
+	{
+		private readonly StoreContext _storeContext;
+		public ProductsController(StoreContext dbContext)
+		{
+			_storeContext = dbContext;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<List<Product>>> GetProducts()
+		{
+			var products = await _storeContext.Products.ToListAsync();
+			return Ok(products);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Product>> GetProduct(int id)
+		{
+			var product = await _storeContext.Products.FindAsync(id);
+			return Ok(product);
+		}
+	}
+}
